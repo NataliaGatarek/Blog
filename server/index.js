f415733e3480fe2649ebc4c333622500a9de10d7
NataliaGatarek <nova.nathalia@gmail.com>
2021-12-07 15:34:28 +0100

    commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex:true
+});
+
+
+app.listen("5000", () => {
+    console.log("Backend is running")
+});
